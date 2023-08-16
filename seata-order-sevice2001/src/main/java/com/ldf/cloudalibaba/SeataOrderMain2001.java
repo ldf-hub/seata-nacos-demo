5949e3436c60c9f3020898cf49cbeec9e1012dd9
ldf-hub <67501985+ldf-hub@users.noreply.github.com>
2023-08-16 15:59:33 +0800

    first commit

---
+++ seata-order-sevice2001/src/main/java/com/ldf/cloudalibaba/SeataOrderMain2001.java
@@ -0,0 +1,22 @@
+package com.ldf.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author w2
+ * @date 2023年8月16日15:49:48
+ */
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源的自动创建
+public class SeataOrderMain2001 {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SeataOrderMain2001.class, args);
+    }
+}
